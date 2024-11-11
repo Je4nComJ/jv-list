e402218d95898cf282b933bb316270d265be9221
Jean Victor <jeanvictor.fa@gmail.com>
2024-11-11 15:37:53 -0300

    Projeto criado

---
+++ src/main/java/com/gomesdevelopers/jvlist/JvlistApplication.java
@@ -0,0 +1,13 @@
+package com.gomesdevelopers.jvlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomesdevelopers/jvlist/JvlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomesdevelopers.jvlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
