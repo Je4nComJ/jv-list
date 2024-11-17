f567cc8b7f2c9f6be77880e28c0c33c03f74f041
Jean Victor <jeanvictor.fa@gmail.com>
2024-11-17 14:41:06 -0300

    move games on list

---
+++ src/main/java/com/gomesdevelopers/jvlist/dto/ReplacementDTO.java
@@ -0,0 +1,14 @@
+package com.gomesdevelopers.jvlist.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReplacementDTO {
+	
+	private Integer sourceIndex = 3;
+	private Integer destinationIndex = 1;
+}
--- src/main/java/com/gomesdevelopers/jvlist/services/GameListService.java
+++ src/main/java/com/gomesdevelopers/jvlist/services/GameListService.java
@@ -9,7 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.gomesdevelopers.jvlist.dto.GameListDTO;
 import com.gomesdevelopers.jvlist.entities.GameList;
+import com.gomesdevelopers.jvlist.projections.GameMinProjection;
 import com.gomesdevelopers.jvlist.repositories.GameListRepository;
+import com.gomesdevelopers.jvlist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -17,10 +19,27 @@ public class GameListService {
 	@Autowired
 	private GameListRepository repository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = repository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).collect(Collectors.toList());		
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			repository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 
 	
